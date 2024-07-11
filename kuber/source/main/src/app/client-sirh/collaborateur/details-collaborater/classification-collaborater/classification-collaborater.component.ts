f4fd52946bd7503988a06961b9a1708698f69c1a
Hanae <hanaebninou28@gmail.com>
2024-07-11 23:36:39 +0100

    create contract and classification components

---
+++ kuber/source/main/src/app/client-sirh/collaborateur/details-collaborater/classification-collaborater/classification-collaborater.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-classification-collaborater',
+  standalone: true,
+  imports: [],
+  templateUrl: './classification-collaborater.component.html',
+  styleUrl: './classification-collaborater.component.scss'
+})
+export class ClassificationCollaboraterComponent {
+
+}
