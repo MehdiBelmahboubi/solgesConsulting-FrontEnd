215b4188ea8d7255aff8291186b58fdf7056e106
Mehdi Belmahboubi <125589865+MehdiBelmahboubi@users.noreply.github.com>
2024-07-12 17:22:27 +0100

    Add Tab to Collaborater Details

--- kuber/source/main/src/app/client-sirh/collaborateur/details-collaborater/details-collaborater.component.ts
+++ kuber/source/main/src/app/client-sirh/collaborateur/details-collaborater/details-collaborater.component.ts
@@ -1,17 +1,17 @@
-import {AfterViewInit, Component, Input, OnInit, Output} from '@angular/core';
-import {FeatherIconsComponent} from "@shared/components/feather-icons/feather-icons.component";
-import {MatButton, MatIconButton} from "@angular/material/button";
-import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
-import {RouterLink, RouterOutlet} from "@angular/router";
-import {BreadcrumbComponent} from "@shared/components/breadcrumb/breadcrumb.component";
-import {MatIcon} from "@angular/material/icon";
-import {MatProgressBar} from "@angular/material/progress-bar";
-import {NgApexchartsModule} from "ng-apexcharts";
-import {NgScrollbar} from "ngx-scrollbar";
-import {NgIf} from "@angular/common";
+import { AfterViewInit, Component, Input, OnInit, Output } from '@angular/core';
+import { FeatherIconsComponent } from "@shared/components/feather-icons/feather-icons.component";
+import { MatButton, MatIconButton } from "@angular/material/button";
+import { MatMenu, MatMenuItem, MatMenuTrigger } from "@angular/material/menu";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { BreadcrumbComponent } from "@shared/components/breadcrumb/breadcrumb.component";
+import { MatIcon } from "@angular/material/icon";
+import { MatProgressBar } from "@angular/material/progress-bar";
+import { NgApexchartsModule } from "ng-apexcharts";
+import { NgScrollbar } from "ngx-scrollbar";
+import { NgClass, NgIf } from "@angular/common";
 import { ContratCollaboraterComponent } from "./contrat-collaborater/contrat-collaborater.component";
 import { ClassificationCollaboraterComponent } from './classification-collaborater/classification-collaborater.component';
-
+import { HeaderSirhClientComponent } from 'app/client-sirh/header-sirh-client/header-sirh-client.component';
 
 export function compare(a: number | string, b: number | string, isAsc: boolean) {
   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
@@ -20,8 +20,9 @@ export function compare(a: number | string, b: number | string, isAsc: boolean) 
 @Component({
   selector: 'app-structure-organisations',
   standalone: true,
-    imports: [
+  imports: [
     FeatherIconsComponent,
+    HeaderSirhClientComponent,
     MatButton,
     MatMenu,
     MatMenuItem,
@@ -33,38 +34,33 @@ export function compare(a: number | string, b: number | string, isAsc: boolean) 
     MatProgressBar,
     NgApexchartsModule,
     NgScrollbar,
+    NgClass,
     NgIf,
     RouterOutlet,
     ContratCollaboraterComponent,
     ClassificationCollaboraterComponent
-],
+  ],
   templateUrl: './details-collaborater.component.html',
-  styleUrl: './details-collaborater.component.scss'
+  styleUrls: ['./details-collaborater.component.scss']
 })
-export class DetailsCollaboraterComponent implements OnInit,AfterViewInit{
-  contratCollaboraterComponent : boolean=true;
-  classificationCollaboraterComponent : boolean =false;
-  componentActive:string ='';
-  
-  @Input() loading =true ;
-  @Output() refreshUnity!:boolean;
+export class DetailsCollaboraterComponent implements OnInit, AfterViewInit {
+  contratCollaboraterComponent: boolean = true;
+  classificationCollaboraterComponent: boolean = false;
+  componentActive: string = '';
 
-  findComponentActive(componentActive:string) {
-    if(componentActive === 'contratCollaboraterComponent'){
-      console.log("holooooo")
-      this.componentActive='contratCollaboraterComponent';
-      this.contratCollaboraterComponent=true;
-      this.classificationCollaboraterComponent=false;
-      
-    }
-    else if(componentActive === 'classificationCollaboraterComponent'){
-      this.componentActive='classificationCollaboraterComponent';
-      this.classificationCollaboraterComponent=true;
-      this.contratCollaboraterComponent=false;
-     
-    }
-    else{
-     console.log("pas de route")
+  @Input() loading = true;
+  @Output() refreshUnity!: boolean;
+
+  findComponentActive(componentActive: string) {
+    this.componentActive = componentActive;
+    if (componentActive === 'contratCollaboraterComponent') {
+      this.contratCollaboraterComponent = true;
+      this.classificationCollaboraterComponent = false;
+    } else if (componentActive === 'classificationCollaboraterComponent') {
+      this.classificationCollaboraterComponent = true;
+      this.contratCollaboraterComponent = false;
+    } else {
+      console.log("pas de route");
     }
   }
 
@@ -72,9 +68,5 @@ export class DetailsCollaboraterComponent implements OnInit,AfterViewInit{
     this.findComponentActive('contratCollaboraterComponent');
   }
 
-  ngAfterViewInit(): void {
-
-  }
-
- 
+  ngAfterViewInit(): void {}
 }
