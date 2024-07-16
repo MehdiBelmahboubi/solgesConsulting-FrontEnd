674d5d583947d962b028a8249ac8ccaa35adc300
Hanae <hanaebninou28@gmail.com>
2024-07-16 03:16:48 +0100

    Update Classification Details

--- kuber/source/main/src/app/client-sirh/collaborateur/details-collaborater/classification-collaborater/classification-collaborater.component.ts
+++ kuber/source/main/src/app/client-sirh/collaborateur/details-collaborater/classification-collaborater/classification-collaborater.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { Collaborater } from 'app/models/collaborater.model';
+import { CollaboraterService } from 'app/services/collaborater.service';
 
 @Component({
   selector: 'app-classification-collaborater',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatDatepickerModule, MatNativeDateModule, MatInputModule, NgIf, MatButtonModule],
   templateUrl: './classification-collaborater.component.html',
   styleUrl: './classification-collaborater.component.scss'
 })
 export class ClassificationCollaboraterComponent {
+  collaborater: Collaborater = new Collaborater();
+  addMode!: Boolean;
+  editMode: boolean = false;
+  constructor(private collaboraterService: CollaboraterService) { }
 
+  ngOnInit(): void {
+    if (history.state && history.state.collaborater) {
+      this.collaborater = history.state.collaborater;
+    } else if (history.state && history.state.collaboraterEdit) {
+      this.collaborater = history.state.collaboraterEdit;
+      this.openEditMode();
+    }
+    else {
+      this.addMode = true;
+    }
+  }
+
+  openEditMode() {
+    this.editMode = true;
+    this.addMode = false;
+  }
+
+  addCollaborater() {
+    this.collaboraterService.addCollaborateur(this.collaborater).subscribe({
+      next: () => {
+        console.log('Collaborateur Added');
+      },
+      error: () => {
+        console.error('Error Adding Collaborateur:');
+      }
+    });
+  }
 }
