1bc274eef569589d990f7445ba8540369973dded
Hanae <hanaebninou28@gmail.com>
2024-07-26 00:33:57 +0100

    Inizializing conges components

---
+++ kuber/source/main/src/app/client-sirh/conge/synthese/synthese.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-synthese',
+  standalone: true,
+  imports: [],
+  templateUrl: './synthese.component.html',
+  styleUrl: './synthese.component.scss'
+})
+export class SyntheseComponent {
+
+}
